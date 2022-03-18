80770e9547dfbea5d48d5d41748723ac97df45ff
garodiaa <96367006+garodiaa@users.noreply.github.com>
2022-03-18 21:15:53 +0600

    Update 1038.c

--- 1038.c
+++ 1038.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-   int z;
-   z=sqrt(4);
-     printf("Total: %d",z);
+    int X , Y ;
+    float T ,arr[]={4.00 ,4.50,5.00,2.00,1.50};
+    scanf("%d %d",&X,&Y);
+     T=(arr[X-1])*Y;
+     printf("Total: R$ %.2f\n",T);
      return 0;
 }
