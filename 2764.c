15014050f74ac90fb83c66a9f22898912bf19c82
garodiaa <96367006+garodiaa@users.noreply.github.com>
2022-08-21 23:23:44 +0600

    Update 2764.c

--- 2764.c
+++ 2764.c
@@ -15,3 +15,16 @@ int main()
     
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string date;
+    cin>>date;
+    cout<<date[3]<<date[4]<<"/"<<date[0]<<date[1]<<"/"<<date[6]<<date[7]<<endl;
+    cout<<date[6]<<date[7]<<"/"<<date[3]<<date[4]<<"/"<<date[0]<<date[1]<<endl;
+    cout<<date[0]<<date[1]<<"-"<<date[3]<<date[4]<<"-"<<date[6]<<date[7]<<endl;
+
+    return 0;
+}
